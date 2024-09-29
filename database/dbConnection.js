a9c8082c0efa30caf0e616f75ca2bf2d48e5ff6f
newfutureistic <rv091625@gmail.com>
2024-09-29 02:10:55 -0700

    commit

---
+++ database/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "NEW_PORTFOLIO",
+    })
+    .then(() => {
+      console.log("Connected to database.");
+    })
+    .catch((error) => {
+      console.log(`Some Error Occured While Connecting To Database: ${error}`);
+    });
+};
+export default dbConnection;
---
+++ models/timelineSchema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const timelineSchema= new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title Required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Discription Required"],
+  },
+  timeline:{
+    from:{
+        type:String,
+        required:[true,"Timeline Starting Date is Required!"]
+    },
+    to:String,
+  }
+});
+export const Timeline = mongoose.model("Timeline", timelineSchema);
